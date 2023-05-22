0a23a499e00360f93278274ec5154945efb3a9af
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-22 14:41:05 -0700

    feat(main): animate rotating square

--- main.js
+++ main.js
@@ -6,11 +6,31 @@ const params = {
 const { body } = document;
 const two = new Two(params).appendTo(body);
 
-const radius = 50;
+// Circle
+// const radius = 50;
 const x = two.width * 0.5;
 const y = two.height * 0.5;
-const circle = two.makeCircle(x, y, radius);
-circle.fill = '#00A6FB';
+// const circle = two.makeCircle(x, y, radius);
+// circle.fill = '#00A6FB';
+// circle.noStroke();
 
+// Rectangle
+const width = 100;
+const height = 100;
+const rect = two.makeRectangle(x, y, width, height);
+rect.fill = '#00A6FB';
+rect.noStroke();
+
+function update() {
+  const angle = Math.PI / 180;
+  const twoPi = 2 * Math.PI;
+  // This code is called every time two.update() is called.
+  rect.rotation += angle;
+  while (rect.rotation > twoPi) {
+    rect.rotation -= twoPi;
+  }
+}
+
+two.bind('update', update);
 // Draw
-two.update();
+two.play();
