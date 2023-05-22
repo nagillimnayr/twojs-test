0a509ca07ddee470ef9c244a36bba6d0702916e9
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-22 14:58:40 -0700

    feat(main): add more rectangles

--- main.js
+++ main.js
@@ -1,36 +1,52 @@
-import Two from 'two.js';
+import Two from "two.js";
 
 const params = {
   fullscreen: true,
+  autostart: true,
 };
 const { body } = document;
 const two = new Two(params).appendTo(body);
 
 // Circle
 // const radius = 50;
-const x = two.width * 0.5;
-const y = two.height * 0.5;
+// const x = two.width * 0.5;
+// const y = two.height * 0.5;
 // const circle = two.makeCircle(x, y, radius);
 // circle.fill = '#00A6FB';
 // circle.noStroke();
 
 // Rectangle
-const width = 100;
-const height = 100;
-const rect = two.makeRectangle(x, y, width, height);
-rect.fill = '#00A6FB';
-rect.noStroke();
+const width = 50;
+const height = 50;
+const rect = two.makeRectangle(0, 0, width * 2, height * 2);
+// rect.fill = "#00A6FB";
+// rect.noStroke();
 
+// Array of rectangles
+const rects = [];
+rects.push(rect);
+
+const numOfRects = 8;
+let angle = (2 * Math.PI) / numOfRects;
+const twoPi = 2 * Math.PI;
+const radius = 200;
+for (let i = 0; i < numOfRects; i += 1) {
+  const x = radius * Math.cos(angle * i);
+  const y = radius * Math.sin(angle * i);
+  rects.push(two.makeRectangle(x, y, width, height));
+}
+
+const group = two.makeGroup(rects);
+group.fill = "#00A6FB";
+group.position.set(two.width / 2, two.height / 2);
+
+angle = Math.PI / 180;
 function update() {
-  const angle = Math.PI / 180;
-  const twoPi = 2 * Math.PI;
   // This code is called every time two.update() is called.
-  rect.rotation += angle;
-  while (rect.rotation > twoPi) {
-    rect.rotation -= twoPi;
+  group.rotation += angle;
+  while (group.rotation > twoPi) {
+    group.rotation -= twoPi;
   }
 }
 
-two.bind('update', update);
-// Draw
-two.play();
+two.bind("update", update);
